26904a054854ea40e89dae57bf60dc5b27514d1a
Hyun Seok JANG <hyun.jang2@student.tafesa.edu.au>
2019-09-19 13:20:45 +0930

    Added ShoppingListInfo page with all working functions.

---
+++ Start-Finance-master/InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class ShoppingList
+    {
+        [PrimaryKey]
+        public string ItemID { get; set; }
+
+        [NotNull]
+        public string ShopName { get; set; }
+
+        [NotNull]
+        public string NameOfItem { get; set; }
+
+        [NotNull]
+        public string ShoppingDate { get; set; }
+
+        [NotNull]
+        public string PriceQuoted { get; set; }
+    }
+}
